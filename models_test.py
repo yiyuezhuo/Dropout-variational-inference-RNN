26ed4d64db84cce94d24a0de2755b3e7e8b78848
yiyuezhuo <616271835@163.com>
2019-11-15 22:50:22 +0800

    refactor code to use flake8

--- models_test.py
+++ models_test.py
@@ -2,16 +2,17 @@
 Ensure our LSTM got same value as original LSTM.
 '''
 
-from models import MyLSTM, BayesLSTM
+from models import MyLSTM   # ,BayesLSTM
 
 import torch
 from torch import nn
-import torch.nn.functional as F
+# import torch.nn.functional as F
 
 lstm_bb = nn.LSTM(6, 4, num_layers=1, bias=True, batch_first=False, dropout=0, bidirectional=False)
-lstm_mock = MyLSTM(lstm_bb.weight_ih_l0.clone(), lstm_bb.weight_hh_l0.clone(), lstm_bb.bias_ih_l0.clone(), lstm_bb.bias_hh_l0.clone())
+lstm_mock = MyLSTM(lstm_bb.weight_ih_l0.clone(), lstm_bb.weight_hh_l0.clone(),
+                   lstm_bb.bias_ih_l0.clone(), lstm_bb.bias_hh_l0.clone())
 
-x_dummy = torch.randn(10,1,6)
+x_dummy = torch.randn(10, 1, 6)
 
 out_bb = lstm_bb(x_dummy)
 out_mock = lstm_mock(x_dummy)
@@ -19,7 +20,7 @@ out_mock = lstm_mock(x_dummy)
 output_bb, (h_bb, c_bb) = out_bb
 output_mock, (h_mock, c_mock) = out_mock
 
-for a,b in zip([output_bb, h_bb, c_bb], [output_mock, h_mock, c_mock]):
+for a, b in zip([output_bb, h_bb, c_bb], [output_mock, h_mock, c_mock]):
     print("Allclosed: ", a.allclose(b), " Norm of diff:", (a-b).norm())
 
 assert lstm_bb.weight_ih_l0.grad is None
@@ -27,7 +28,7 @@ h_bb.sum().backward()
 assert lstm_mock.weight_ih.grad is None
 h_mock.sum().backward()
 
-optimizer_bb =  torch.optim.SGD(lstm_bb.parameters(), 0.1)
+optimizer_bb = torch.optim.SGD(lstm_bb.parameters(), 0.1)
 optimizer_mock = torch.optim.SGD(lstm_mock.parameters(), 0.1)
 
 assert lstm_bb.weight_hh_l0.allclose(lstm_mock.weight_hh)
